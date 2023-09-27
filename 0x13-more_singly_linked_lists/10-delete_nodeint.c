b9f2374d91d3b40b92de1f351b234ac7bdb3743c
HellenTrotter <m.hbodiba@gmail.com>
2023-09-26 20:55:24 -0700

    Delete node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - a function that deletes the node at index
+ * index of a listint_t linked list.
+ * @head: parameter
+ * @index: parameter
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int k;
+	listint_t *temp;
+	listint_t *new;
+
+	temp = *head;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	for (k = 0; k < index && temp != NULL && index != 0; k++)
+		temp = (*head)->next;
+
+	if (temp == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		new = temp->next;
+		free(temp);
+		*head = new;
+	}
+	else
+	{
+		if (temp->next == NULL)
+			new = temp->next;
+		else
+			new = temp->next->next;
+		free(temp->next);
+		temp->next = new;
+	}
+	return (1);
+}
