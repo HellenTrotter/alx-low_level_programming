cfb2d96bdeb7bb33a0aef38c25aa1544853d14d2
Hellen Trotter <m.hbodiba@gmail.com>
2023-12-22 06:02:14 -0800

    Create hash table update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,28 +8,28 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int k;
+	unsigned long int k = 0;
 	hash_table_t *h_table = malloc(sizeof(hash_table_t));
 
-	if (h_table == NULL)
+	if (!h_table)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
+	}
+
+	h_table->array = malloc(sizeof(hash_node_t **) * size);
+
+	if (!h_table->array)
+	{
+		free(h_table);
+		return (NULL);
+	}
+
+	while (k < size)
+	{
+		h_table->array[k] = NULL;
+		k++;
 	}
 
 	h_table->size = size;
-	h_table->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (h_table->array == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		return (NULL);
-	}
-
-	for (k = 0; k < size; k++)
-	{
-		h_table->array[k] = NULL;
-	}
-
 	return (h_table);
 }
